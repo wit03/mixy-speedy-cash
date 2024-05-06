283086c31c635eb7d81f7dc0fa434f4e857a2dab
wit03 <witjarukit@gmail.com>
2024-05-06 19:11:25 +0700

    feat: amount field

--- frontend/src/app/(nonavbar)/transfer/_components/amountField.tsx
+++ frontend/src/app/(nonavbar)/transfer/_components/amountField.tsx
@@ -1,20 +1,20 @@
 "use client"
 
-import { PatternFormat } from "react-number-format";
+import { NumericFormat } from 'react-number-format';
 
 interface AddrFieldProps {
-    setAddr: React.Dispatch<React.SetStateAction<number | undefined>>;
-    Addr: number | undefined;
+    setAmount: React.Dispatch<React.SetStateAction<number | undefined>>;
+    Amount: number | undefined;
 }
 
-const AddrField = (props: AddrFieldProps) => {
+const AmountField = (props: AddrFieldProps) => {
 
     return (
         <div className="relative h-11 w-full min-w-[200px]">
-            <PatternFormat displayType="input" placeholder="XXX-XXX-XX-X" format="###-###-##-#" value={props.Addr} onChange={(e) => props.setAddr(+e.target.value)}
+            <NumericFormat displayType="input" placeholder="" thousandSeparator=","  value={props.Amount} onChange={(e) => props.setAmount(+e.target.value)}
                 className="peer h-full w-full border-b border-gray-400 bg-transparent pt-4 pb-1.5 font-sans text-2xl font-normal text-blue-gray-900 outline outline-0 transition-all placeholder-shown:border-gray-800 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
         </div>
     )
 }
 
-export default AddrField;
+export default AmountField;
