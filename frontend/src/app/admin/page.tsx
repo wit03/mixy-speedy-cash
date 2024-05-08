5101e193e464e7e54dab522f3cfff2a09df512ef
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-05-08 20:31:19 +0700

    remove: unnecessary css style

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -18,11 +18,11 @@ export default function Page(){
         </svg>
         <div id="logo" className="flex flex-col items-center justify-center">
           <svg xmlns="http://www.w3.org/2000/svg" fill="#372A54" className="w-[140px] h-[140px]" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd"/></svg>
-          <h6 className="w-91px relative text-base text-right font-semibold font-rubik text-[#372A54] inline-block">EMPLOYEE</h6>
+          <h6 className="w-91px relative text-base text-right font-semibold font-rubik text-[#372A54]">EMPLOYEE</h6>
         </div>
         <form>
           <div className="flex flex-col w-[20rem] gap-3.5 ">
-            <label className="relative text-16 tracking-0.02 font-rubik text-black text-left inline-block">Employee ID</label>
+            <label className="relative text-16 tracking-0.02 font-rubik text-black text-left">Employee ID</label>
             <div className="w-full relative">
               <UsernameInput
               value={email}
@@ -30,14 +30,14 @@ export default function Page(){
               />
             </div>
           
-            <label className="relative text-16 tracking-0.02 font-rubik text-black text-left inline-block">Password</label>
+            <label className="relative text-16 tracking-0.02 font-rubik text-black text-left">Password</label>
             <div className="w-full relative">
               <PasswordInput
               value={password}
               setValue={setPassword}
               />
             </div>
-            <a type="button" className="w-full relative text-13 tracking-0.02 font-rubik text-grey text-right inline-block">Forget Password ?</a>
+            <a type="button" className="w-full relative text-13 tracking-0.02 font-rubik text-grey text-right">Forget Password ?</a>
             <button type="submit" className="relative w-full h-[37px] bg-gradient-to-r from-[#a6c1ee] to-[rgba(122,93,186,0.2)] border border-solid border-ded4f5 box-border rounded-md text-white text-14 tracking-0.02 font-rubik text-center inline-block">Login</button>
             </div>
           </form>
