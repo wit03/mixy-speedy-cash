390e852bc5ec3e8eb54249f84e828a751dad3653
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-05-12 13:09:19 +0700

    feat: add details to the loan information page

---
+++ frontend/src/app/(navbar)/loan/normal/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import Link from "next/link";
+
+const LoanInformation = () => {
+    return (
+        <div className="flex flex-col m-4 font-rubik items-start mx-6 mt-14 mb-6">
+            <svg className="absolute top-0 right-0" xmlns="http://www.w3.org/2000/svg" width="124" height="126" viewBox="0 0 124 126" fill="none"><circle cx="100" cy="26" r="100" fill="#F8E192"></circle></svg>
+            <svg className="absolute bottom-20 left-0 z-0" xmlns="http://www.w3.org/2000/svg" width="128" height="200" viewBox="0 0 128 200" fill="none"><circle cx="28" cy="100" r="100" fill="#2FCBFC4D" /></svg>
+            <Link href="/loan" aria-label="Back to Home Page">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 stroke-current text-[#858585CC]">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"></path>
+                </svg>
+            </Link>
+            <div className="relative text-[20px] tracking-0.02 font-semibold inline-block py-2 mt-2">Normal Loan</div>
+            <img
+                src="/loan/normalLoan.png"
+                alt="AJARN DANG"
+                className="relative w-full rounded-sm overflow-hidden mt-[20px]"
+            />
+            <div className="relative w-full rounded-sm bg-[rgba(122,138,237,0.15)] pt-[13px] px-[18px] mt-[21px]">
+                <div className="font-bold">Information</div>
+                <div className="w-282 px-4 py-2 font-rubik text-sm font-light text-left text-black">
+                    <p className="mb-4">Objective: The purpose of this policy is to outline the terms and conditions for borrowing a loan with a fixed interest rate of 3%, to be repaid within six months.</p>
+                    <p className="mb-2">Eligibility Criteria:</p>
+                    <ol className="list-decimal ml-4">
+                        <li className="mb-2">Age: The borrower must be of legal age as per the jurisdiction's laws.</li>
+                        <li className="mb-2">Creditworthiness: Borrowers must meet the credit requirements set by the lending institution.</li>
+                        <li className="mb-2">Income Stability: Borrowers must demonstrate a stable source of income to ensure repayment capability.</li>
+                        <li>Documentation: Borrowers are required to provide necessary documentation as per the lending institution's requirements.</li>
+                    </ol>
+                </div>
+            </div>
+            <button type="button" className="mt-[21px] relative w-full h-[37px] bg-gradient-to-r from-[#a6c1ee] to-[rgba(122,93,186,0.2)] border border-solid border-ded4f5 box-border rounded-md text-white text-14 tracking-0.02 font-bold text-center inline-block">Applying Loan</button>
+        </div >
+    )
+}
+
+export default LoanInformation
--- frontend/src/app/(navbar)/loan/page.tsx
+++ frontend/src/app/(navbar)/loan/page.tsx
@@ -1,34 +1,36 @@
 "use client"
 
-import { makeRequest } from "@/hook/makeRequets"
-import { useState } from "react"
 import LoanBox from "./_component/LoanBox"
-import toast from "react-hot-toast"
-import { useRouter } from 'next/navigation'
+import Link from "next/link";
 
 const HomePage = () => {
-  return (
-    <div className="flex flex-col m-4 font-rubik items-start mx-6 mt-14 mb-6">
-        <svg className="absolute top-0 right-0" xmlns="http://www.w3.org/2000/svg" width="124" height="126" viewBox="0 0 124 126" fill="none"><circle cx="100" cy="26" r="100" fill="#F8E192"></circle></svg>
-            <svg className="absolute bottom-20 left-0 z-0" xmlns="http://www.w3.org/2000/svg" width="128" height="200" viewBox="0 0 128 200" fill="none"><circle cx="28" cy="100" r="100" fill="#2FCBFC4D"/></svg>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" className="w-6 h-6" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"/>
-        </svg>
-        <div className="relative text-[20px] tracking-0.02 font-semibold inline-block py-2">Personal Loan</div>
-        <div className="relative text-[15px] tracking-0.02 font-light">Select the loan that suit with you</div>
-        <LoanBox 
-            title="Normal Loan"
-            interestRate={3}
-            maximumLoan="<= 50000"
-        />
-        <LoanBox 
-            title="Special Loan"
-            interestRate={15}
-            maximumLoan='Unlimited'
-        />
-        
-    </div>
-  )
+    return (
+        <div className="flex flex-col m-4 font-rubik items-start mx-6 mt-14 mb-6">
+            <svg className="absolute top-0 right-0" xmlns="http://www.w3.org/2000/svg" width="124" height="126" viewBox="0 0 124 126" fill="none"><circle cx="100" cy="26" r="100" fill="#F8E192"></circle></svg>
+            <svg className="absolute bottom-20 left-0 z-0" xmlns="http://www.w3.org/2000/svg" width="128" height="200" viewBox="0 0 128 200" fill="none"><circle cx="28" cy="100" r="100" fill="#2FCBFC4D" /></svg>
+            <Link href="/" aria-label="Back to Home Page">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 stroke-current text-[#858585CC]">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"></path>
+                </svg>
+            </Link>
+            <div className="relative text-[20px] tracking-0.02 font-semibold inline-block py-2 mt-2">Personal Loan</div>
+            <div className="relative text-[15px] tracking-0.02 font-light">Select the loan that suit with you</div>
+            <Link href="/loan/normal" className="w-full">
+                <LoanBox
+                    title="Normal Loan"
+                    interestRate={3}
+                    maximumLoan="<= 50000"
+                    imageSrc="./loan/normalLoan.png"
+                />
+            </Link>
+            <LoanBox
+                title="Special Loan"
+                interestRate={15}
+                maximumLoan='Unlimited'
+                imageSrc="./loan/specialLoan.jpg"
+            />
+        </div>
+    )
 }
 
 export default HomePage
