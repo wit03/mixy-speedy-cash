26ebe50cacbc1784ee517ac0f3dbce20b8e57d43
wit03 <witjarukit@gmail.com>
2024-05-25 18:31:23 +0700

    feat: banker dashboard

--- frontend/src/app/(nonavbar)/employee/page.tsx
+++ frontend/src/app/(nonavbar)/employee/page.tsx
@@ -1,15 +1,255 @@
+"use client"
+
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React, { PureComponent } from 'react';
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
 export default function page({
 
-}:{
-    
-})  {
+}: {
 
-return (
+  }) {
 
-    <> 
-    Graph
-    </>
+  return (
 
-    )
+    <div className='mx-10'>
+      <h1 className=' font-rubik text-4xl my-8'>Dashboard</h1>
+      <div className="grid w-full grid-cols-5 gap-6">
+        <div className="min-h-80 bg-white rounded-xl col-span-2 w-full grid grid-cols-2">
+
+          <div className='flex '>
+            <div>icon</div>
+            <div className='flex flex-col'>
+              <div>Total Bank Account</div>
+              <div> 500 People</div>
+            </div>
+          </div>
+
+          <div className='flex '>
+            <div>icon</div>
+            <div className='flex flex-col'>
+              <div>Total Bank Account</div>
+              <div> 500 People</div>
+            </div>
+          </div>
+
+          <div className='flex '>
+            <div>icon</div>
+            <div className='flex flex-col'>
+              <div>Total Bank Account</div>
+              <div> 500 People</div>
+            </div>
+          </div>
+
+          <div className='flex '>
+            <div>icon</div>
+            <div className='flex flex-col'>
+              <div>Total Bank Account</div>
+              <div> 500 People</div>
+            </div>
+          </div>
+
+          <div className='flex '>
+            <div>icon</div>
+            <div className='flex flex-col'>
+              <div>Total Bank Account</div>
+              <div> 500 People</div>
+            </div>
+          </div>
+
+          <div className='flex '>
+            <div>icon</div>
+            <div className='flex flex-col'>
+              <div>Total Bank Account</div>
+              <div> 500 People</div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="bg-white rounded-2xl p-6 col-span-3 w-full">
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+              <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+            </BarChart>
+          </ResponsiveContainer>
+
+        </div>
+
+        <div className="bg-white rounded-2xl p-6 col-span-2 w-full min-h-60">
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+              <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+            </BarChart>
+          </ResponsiveContainer>
+
+        </div>
+
+        <div className="bg-white rounded-2xl p-6 col-span-3 w-full">
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+              <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+            </BarChart>
+          </ResponsiveContainer>
+
+        </div>
+
+      </div>
+      <div className="relative overflow-x-auto mt-6">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Product name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Color
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                Apple MacBook Pro 17
+              </th>
+              <td className="px-6 py-4">
+                Silver
+              </td>
+              <td className="px-6 py-4">
+                Laptop
+              </td>
+              <td className="px-6 py-4">
+                $2999
+              </td>
+            </tr>
+            <tr className="bg-white border-b ">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                Microsoft Surface Pro
+              </th>
+              <td className="px-6 py-4">
+                White
+              </td>
+              <td className="px-6 py-4">
+                Laptop PC
+              </td>
+              <td className="px-6 py-4">
+                $1999
+              </td>
+            </tr>
+            <tr className="bg-white ">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                Magic Mouse 2
+              </th>
+              <td className="px-6 py-4">
+                Black
+              </td>
+              <td className="px-6 py-4">
+                Accessories
+              </td>
+              <td className="px-6 py-4">
+                $99
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  )
 
 }
