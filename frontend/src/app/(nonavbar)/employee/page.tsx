2130c04c1206ddbd7f2bee80e2e884a741ef180e
Mark169Z <ksd.markmax@hotmail.com>
2024-05-25 19:59:39 +0700

    Icon added

--- frontend/src/app/(nonavbar)/employee/page.tsx
+++ frontend/src/app/(nonavbar)/employee/page.tsx
@@ -59,51 +59,79 @@ export default function page({
     <div className='mx-10'>
       <h1 className=' font-rubik text-4xl my-8'>Dashboard</h1>
       <div className="grid w-full grid-cols-5 gap-6">
-        <div className="min-h-80 bg-white rounded-xl col-span-2 w-full grid grid-cols-2">
+        <div className="items-center min-h-80 bg-white rounded-xl col-span-2 w-full grid grid-cols-2">
 
-          <div className='flex '>
-            <div>icon</div>
-            <div className='flex flex-col'>
+          <div className='flex justify-center items-center'>
+            <div className='bg-[#D9D9D9] size-10 flex justify-center items-center mx-2 rounded-md'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#9747FF" className="size-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+              </svg>
+            </div>
+
+            <div className='flex flex-col justify-center'>
               <div>Total Bank Account</div>
               <div> 500 People</div>
             </div>
           </div>
 
-          <div className='flex '>
-            <div>icon</div>
-            <div className='flex flex-col'>
+          <div className='flex justify-center items-center'>
+            <div className='bg-[#D9D9D9] size-10 flex justify-center items-center mx-2 rounded-md'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#9747FF" className="size-9">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
+              </svg>
+
+            </div>
+            <div className='flex flex-col justify-center'>
               <div>Total Bank Account</div>
               <div> 500 People</div>
             </div>
           </div>
 
-          <div className='flex '>
-            <div>icon</div>
-            <div className='flex flex-col'>
+          <div className='flex justify-center items-center'>
+            <div className='bg-[#D9D9D9] size-10 flex justify-center items-center mx-2 rounded-md'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#9747FF" className="size-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+              </svg>
+            </div>
+            <div className='flex flex-col justify-center'>
               <div>Total Bank Account</div>
               <div> 500 People</div>
             </div>
           </div>
 
-          <div className='flex '>
-            <div>icon</div>
-            <div className='flex flex-col'>
+          <div className='flex justify-center items-center'>
+            <div className='bg-[#D9D9D9] size-10 flex justify-center items-center mx-2 rounded-md'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#9747FF" className="size-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+              </svg>
+            </div>
+            <div className='flex flex-col justify-center'>
               <div>Total Bank Account</div>
               <div> 500 People</div>
             </div>
           </div>
 
-          <div className='flex '>
-            <div>icon</div>
-            <div className='flex flex-col'>
+          <div className='flex justify-center items-center'>
+            <div className='bg-[#D9D9D9] size-10 flex justify-center items-center mx-2 rounded-md'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#9747FF" className="size-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+              </svg>
+
+            </div>
+            <div className='flex flex-col justify-center'>
               <div>Total Bank Account</div>
               <div> 500 People</div>
             </div>
           </div>
 
-          <div className='flex '>
-            <div>icon</div>
-            <div className='flex flex-col'>
+          <div className='flex justify-center items-center'>
+            <div className='bg-[#D9D9D9] size-10 flex justify-center items-center mx-2 rounded-md'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#9747FF" className="size-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+              </svg>
+
+            </div>
+            <div className='flex flex-col justify-center'>
               <div>Total Bank Account</div>
               <div> 500 People</div>
             </div>
